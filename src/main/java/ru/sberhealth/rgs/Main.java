e5e44f37f1a8d43d7450350691b9274bf3c7be14
alobov <alobov@sberhealth.ru>
2023-10-23 12:10:25 +0300

    Примеры работы с методами

--- src/main/java/ru/sberhealth/rgs/Main.java
+++ src/main/java/ru/sberhealth/rgs/Main.java
@@ -1,4 +1,21 @@
 package ru.sberhealth.rgs;
 
 public class Main {
+
+    public static void main(String[] args) {
+        addTwoNumbers(1, 2);
+    }
+
+
+    public static int addTwoNumbers(int a, int b){
+        // тело метода
+        int sum = a + b;
+        System.out.println("Сумма a и b равняется " + sum);
+        return sum;
+    }
+
+    public static void addTwoNumbersAndPrintResult(int a, int b){
+        int sum = a + b;
+        System.out.println(sum);
+    }
 }
