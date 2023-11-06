3c8e8cc66f463575aa409c40fc3e29368b531ce0
divanov <forpaymen7romanroro@yandex.ru>
2023-11-06 15:59:23 +0300

    add comments to enum class

--- src/main/java/ru/sberhealth/rgs/CarTypes.java
+++ src/main/java/ru/sberhealth/rgs/CarTypes.java
@@ -3,6 +3,8 @@ package ru.sberhealth.rgs;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+// добавил для примера геттер с использованием библиотеки Lombok, как в проекте.
+// Lombok позволяет значительно сократить ко-во кода используя аннотации
 @Getter
 @AllArgsConstructor
 public enum CarTypes {
