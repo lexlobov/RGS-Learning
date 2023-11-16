948d9e2ff9fd239a421f8f83ddd1fbd59c2fdcad
divanov <forpaymen7romanroro@yandex.ru>
2023-11-17 00:07:41 +0300

    add fibbonachi method

--- src/main/java/ru/sberhealth/rgs/Main.java
+++ src/main/java/ru/sberhealth/rgs/Main.java
@@ -1,4 +1,21 @@
 package ru.sberhealth.rgs;
 
+import java.util.Scanner;
+import  ru.sberhealth.rgs.Homework;
+
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter count of Fibbonachi iterations: ");
+
+        int iterations = scanner.nextInt();
+
+        int result = Homework.getSumResult(iterations);
+        System.out.println(result);
+    }
+
+
 }
