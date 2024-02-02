a241f470e5f84a7ca4e497dff73b066def1862c4
alobov <alobov@sberhealth.ru>
2024-02-02 08:52:31 +0300

    first

---
+++ src/test/java/CalculatorTests.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.sberhealth.rgs.Calculator;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class CalculatorTests {
+
+    private Calculator calculator;
+
+    @BeforeEach
+    // данный метод будет выполняться перед каждым тестовым методом
+    public void setUp(){
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void additionPositiveTest(){
+        int result = calculator.addTwoNumbers(1, 1);
+        assertThat("1 + 1 should be 2", result, equalTo(2));
+    }
+
+    @Test
+    // Данный тест упадет, так как результат равен 3, а мы ожидаем увидеть 2, в результате происходит AssertionError, и тест считается не пройденным
+    public void additionNegativeTest(){
+        int result = calculator.addTwoNumbers(1, 2);
+        assertThat("1 + 1 should be 2", result, equalTo(2));
+    }
+}
