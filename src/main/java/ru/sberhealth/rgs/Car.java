63c1eab2d965ef7278b0191cc9dcdc7657a0a0ae
msemin <syomin1maxim@gmail.com>
2024-02-02 12:12:19 +0300

    Почти, вроде ))

---
+++ src/main/java/ru/sberhealth/rgs/Car.java
@@ -0,0 +1,47 @@
+package ru.sberhealth.rgs;
+
+public class Car {
+    private String model;
+    private int horsepower;
+    private String type;
+    private int numberOfWheels;
+
+    public Car(String model, int horsepower) {
+        this.model = model;
+        this.horsepower = horsepower;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getHorsepower() {
+        return horsepower;
+    }
+
+    public void setHorsepower(int horsepower) {
+        this.horsepower = horsepower;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getNumberOfWheels() {
+        return numberOfWheels;
+    }
+
+    public void setNumberOfWheels(int numberOfWheels) {
+        this.numberOfWheels = numberOfWheels;
+    }
+
+}
+
--- src/main/java/ru/sberhealth/rgs/Main.java
+++ src/main/java/ru/sberhealth/rgs/Main.java
@@ -1,4 +1,36 @@
 package ru.sberhealth.rgs;
 
+import java.lang.reflect.Array;
+
 public class Main {
+    public static void main(String[] args) {
+        Car passengerCar = new Car("Opel", 115);
+        Car freightCar = new Car("Ford", 150);
+
+        passengerCar.setType("Легковой");
+        freightCar.setType("Грузовой");
+
+        System.out.println("Модель автомобиля 1: " + passengerCar.getModel());
+        System.out.println("Лошадиные силы автомобиля 1: " + passengerCar.getHorsepower());
+        System.out.println("Тип автомобиля 1: " + passengerCar.getType());
+
+        System.out.println("Модель автомобиля 2: " + freightCar.getModel());
+        System.out.println("Лошадиные силы автомобиля 2: " + freightCar.getHorsepower());
+        System.out.println("Тип автомобиля 2: " + freightCar.getType());
+
+//        Double calcAver = Utils.calculateAverage(1,2,3);
+        String concStr = Utils.concatenateStrings("При", "вет");
+//        Integer calcSum = Utils.calculateSum(1,2,3);
+//        System.out.println(calcAver);
+        System.out.println(concStr);
+//        System.out.println(calcSum);
+    }
 }
+// Создать в этом классе метод main, в котором показать работу своих классов и методов
+
+// Создать класс с описанием автомобиля. У него должны быть поля для обозначения модели, количества лошадиных сил,
+// типа (легковой или грузовой), количества колес. Создать конструктор по полям модель и количество лошадиных сил, создать геттеры и сеттеры для всех полей.
+// Сделать так, чтобы доступ к полям был возможен только внутри класса. Создать два экземпляра класса, установить им тип и вывести на экран значения их полей.
+
+// Создать класс, который будет хранить в себе статические методы: метод для расчета среднего арифметического элементов целочисленного массива,
+// метод для склеивания любого количества строк в одну, метод для расчета суммы всех входящих в массив элементов.
