a6fd806729b93f2dba9f18b71092d41b6f2c67ff
vrumyantsev <vrumyantsev@sberhealth.ru>
2024-02-16 11:57:10 +0300

    Inheritance_HW_VRumyantsev

--- src/main/java/ru/sberhealth/rgs/Main.java
+++ src/main/java/ru/sberhealth/rgs/Main.java
@@ -4,10 +4,10 @@ import ru.sberhealth.rgs.Garage;
 public class Main {
     public static void main(String[] args) {
         Garage garage = new Garage();
-        garage.addVehicle("Boat", 1); // Добавляет 3 автомобиля в гараж
-        garage.addVehicle("Truck", 3); // Добавляет 3 автомобиля в гараж
-        garage.addVehicle("Plane", 4); // Добавляет 3 автомобиля в гараж
-        garage.removeVehicle("Plane", 2); // Добавляет 2 мотоцикла в гараж
-        garage.showVehiclesInTheGarage(); // Выводит информацию о наличии транспортных средств в гараже
+        garage.addVehicle("Boat", 1);
+        garage.addVehicle("Truck", 3);
+        garage.addVehicle("Plane", 4);
+        garage.removeVehicle("Plane", 2);
+        garage.showVehiclesInTheGarage();
     }
 }
